705c2a6867c5036a8ad14923e3db781133b04f4a
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-07-12 09:02:38 +0530

    js

---
+++ js/lesson3.js
@@ -0,0 +1,6 @@
+var name_message= "Eneter your name"
+var Name = prompt(name_message);
+var age = prompt("Enter your age ");
+var weight = prompt("Enter your weight ");
+
+alert("name : "+ Name+ "\nage  : "+ age+ "\nweight : "+ weight);
